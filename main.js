e3e11c3cfbfe4ebf393a2cb69a7bd5a2454dfcf9
ABeginnerCoderCoding <93920880+ABeginnerCoderCoding@users.noreply.github.com>
2023-03-10 20:39:53 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+function change1() {
+  document.getElementById("img1").src =
+    "https://m.media-amazon.com/images/I/513ZwADqpZL._AC_.jpg";
+}
+function change2() {
+  document.getElementById("img1").src =
+    "https://i.etsystatic.com/22167368/r/il/d0e197/2376204592/il_fullxfull.2376204592_gorz.jpg";
+}
+function change3() {
+  document.getElementById("img2").src =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuZkt_6fpzBRmnnmteaoABrOsYjmc2ARfVqA&usqp=CAU";
+}
+function change4() {
+  document.getElementById("img2").src =
+    "https://sm.ign.com/ign_me/gallery/v/view-all-t/view-all-tapu-lele-gx-pokemon-cards_w1aq.jpg";
+}
+function change5() {
+  document.getElementById("img3").src =
+    "https://cdn1.dotesports.com/wp-content/uploads/2019/06/06100440/Espeon-Deoxys-GX-753x1024.jpg";
+}
+function change6() {
+  document.getElementById("img3").src =
+    "https://m.media-amazon.com/images/I/61ANH++fh-L._AC_.jpg";
+}
+function change7() {
+  document.getElementById("img4").src =
+    "https://assets.pokemon.com/assets/cms2/img/cards/web/BW11/BW11_EN_29.png";
+}
+function 
